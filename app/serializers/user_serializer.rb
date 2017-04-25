53f8beb0e89d6237d53d5561992840f1e018e731
OliviaVF <opvf22@gmail.com>
2017-04-25 10:34:13 +0100

    last changes before pres

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,7 +2,7 @@ class UserSerializer < ActiveModel::Serializer
   has_many :followers
   has_many :following
   has_many :pylons
-  attributes :id, :name, :username, :email, :image_src, :follower_ids, :pylons
+  attributes :id, :name, :username, :email, :image_src, :follower_ids, :following_ids, :pylons
 
   def image_src
     object.image.url
